85171789d6059879fa6c03ad34fd2f2c5dbc5845
Bogdan Rus <rusbogdanclaudiu@yahoo.com>
2017-11-26 20:48:45 +0200

    introduce syslog

--- hello-world.js
+++ hello-world.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+
+var Syslog = require('node-syslog');
+
+Syslog.init("node-syslog", Syslog.LOG_PID | Syslog.LOG_ODELAY, Syslog.LOG_LOCAL0);
+Syslog.log(Syslog.LOG_INFO, "Node Syslog Module output " + new Date());
+//Syslog.close();
+
 // Load the http module to create an http server.
 var http = require('http');
 
