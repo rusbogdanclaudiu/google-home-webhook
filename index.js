c1da93a1592b7efe717f522a1f4815b9e0efe21b
Bogdan Rus <rusbogdanclaudiu@yahoo.com>
2017-11-27 23:35:42 +0200

    webhook

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-var syslog = require("syslog-client");
-var sys = syslog.createClient("127.0.0.1");
-sys.log("example message");
-
+require('rconsole');
+var sys = console;
+sys.set({ facility : 'local0', title: 'basic' });
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -15,6 +14,7 @@ restService.use(bodyParser.json());
 restService.get('/', function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("Hello snapcrafter\n");
+    sys.log('index request');
 });
 
 restService.post('/hook', function (req, res) {
@@ -22,40 +22,13 @@ restService.post('/hook', function (req, res) {
     sys.log('hook request');
 
     try {
-        var speech = 'empty speech';
 
         if (req.body) {
             var requestBody = req.body;
-
-            if (requestBody.result) {
-                speech = '';
-
-                if (requestBody.result.fulfillment) {
-                    speech += requestBody.result.fulfillment.speech;
-                    speech += ' ';
-                }
-
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action + ' ';
-                }
-
-                var parameters = requestBody.result.parameters;
-                if (parameters){
-                    for (var p in parameters){
-                        if(parameters.hasOwnProperty(p) ) {
-                            speech += p + ": " + parameters[p] + "; ";
-                        }
-                    }
-                }
-            }
+            sys.log(requestBody);
         }
 
-        sys.log('result: ' + speech);
-
         return res.json({
-            speech: speech,
-            displayText: speech,
-            source: 'apiai-webhook-sample'
         });
     } catch (err) {
         sys.log("Can't process request: " + err);
